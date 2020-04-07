2c87df65ec648cf26c7224e88d9062a7f1ac0cd6
Quanhu Sheng <shengqh@gmail.com>
2020-04-07 16:42:01 -0500

    add report

--- cpdseqer/report_utils.py
+++ cpdseqer/report_utils.py
@@ -7,9 +7,27 @@ import shutil
 
 from .CategoryItem import CategoryItem
 from .common_utils import check_file_exists, get_reference_start, runCmd, readFileMap, checkFileMap, remove_chr
+from .__version__ import __version__
 
-def report(logger, configFile, outputFilePrefix, block, dbVersion):
-  check_file_exists(configFile)
+def report(logger, sampleListFile, groupDefinitionFile, outputFilePrefix, block, dbVersion):
+  check_file_exists(sampleListFile)
+  check_file_exists(groupDefinitionFile)
+
+  sampleMap = readFileMap(sampleListFile)
+  groupMap = readFileMap(groupDefinitionFile)
+
+  targetFolder = os.path.dirname(outputFilePrefix)
+
+  checkFileMap(sampleMap)
+
+  countFileMap = {sample:sampleMap[sample] + ".count" for sample in sampleMap.keys()}
+  checkFileMap(countFileMap)
+
+  sampleGroupMap = {}
+  for sampleName in sampleMap.keys():
+    if sampleName not in groupMap:
+      raise Exception("Sample %s not in group definition file %s" % (sampleName, groupDefinitionFile))
+    sampleGroupMap[sampleName] = [sampleMap[sampleName], groupMap[sampleName]]
 
   rmdScript = os.path.join( os.path.dirname(__file__), "report.rmd")
   if not os.path.exists(rmdScript):
@@ -50,59 +68,68 @@ def report(logger, configFile, outputFilePrefix, block, dbVersion):
         chromStart = chromStart + block
 
   logger.info("Preparing dinucleotide files ...")
-  targetFolder = os.path.dirname(outputFilePrefix)
   targetConfigFile = os.path.join(targetFolder, "dinucleotide_file.list")
   with open(targetConfigFile, "wt") as fout:
-    with open(configFile, "rt") as fin:
-      for line in fin:
-        parts = line.rstrip().split('\t')
-        dinuFile = parts[0]
-        dinuName = parts[1]
-        dinuGroup = parts[2]
-        targetDinuFile = os.path.join(targetFolder, "%s_block%d.txt" % (os.path.basename(dinuFile), block))
-        fout.write("%s\t%s\t%s\n" % (targetDinuFile, dinuName, dinuGroup))
+    fout.write("Group\tSample\tDinuFile\tCountFile\n")
+    for dinuName in sorted(sampleGroupMap.keys()):
+      dinuFile = sampleGroupMap[dinuName][0]
+      dinuGroup = sampleGroupMap[dinuName][1]
+      targetDinuFile = os.path.join(targetFolder, "%s_block%d.txt" % (os.path.basename(dinuFile), block))
+      targetCountFile = os.path.join(targetFolder, "%s_count.txt" % os.path.basename(dinuFile))
+      fout.write("%s\t%s\t%s\t%s\n" % (dinuGroup, dinuName, os.path.basename(targetDinuFile), os.path.basename(targetCountFile)))
 
-        if os.path.isfile(targetDinuFile):
-          continue
+      if os.path.isfile(targetDinuFile) and os.path.isfile(targetCountFile) :
+        continue
 
-        idxFile = dinuFile + ".tbi"
+      shutil.copyfile(countFileMap[dinuName], targetCountFile)
 
-        check_file_exists(dinuFile)
-        check_file_exists(idxFile)
+      idxFile = dinuFile + ".tbi"
 
-        logger.info("Preparing %s ..." % dinuFile)
-        with open(targetDinuFile, "wt") as fdinu:
-          tb = tabix.open(dinuFile)
+      check_file_exists(dinuFile)
+      check_file_exists(idxFile)
 
-          count = 0
-          lastChrom = ""
-          for catItem in catItems:
-            count = count + 1
-            if count % 10000 == 0:
-              logger.info("%d / %d" % (count, len(catItems)))
+      dinuMap = {}
+      logger.info("Preparing %s ..." % dinuFile)
+      with open(targetDinuFile, "wt") as fdinu:
+        tb = tabix.open(dinuFile)
 
-            if catItem.reference_name != lastChrom:
-              if lastChrom in chromLengthMap:
-                fdinu.write("%s\t%d\t%d\t%d\n" % (lastChrom, chromLengthMap[lastChrom] - 1, chromLengthMap[lastChrom], 0))
-              lastChrom = catItem.reference_name
-            
-            #logger.info("Processing %s:%d-%d ..." %(catItem.reference_name, catItem.reference_start, catItem.reference_end))
-            tbiter = tb.query(catItem.reference_name, catItem.reference_start, catItem.reference_end)
-            records = [record for record in tbiter]
-            totalCount = 0
-            for record in records:
-              dinucleotide = record[3]
-              diCount = int(record[4])
-              if diCount == 0:
-                diCount = 1
-              totalCount += diCount
-            
-            if totalCount > 0:
-              fdinu.write("%s\t%d\t%d\t%d\n" % (catItem.reference_name, catItem.reference_start, catItem.reference_end, totalCount))
-          fdinu.write("%s\t%d\t%d\t%d\n" % (lastChrom, chromLengthMap[lastChrom] - 1, chromLengthMap[lastChrom], 0))
+        count = 0
+        lastChrom = ""
+        for catItem in catItems:
+          count = count + 1
+          if count % 10000 == 0:
+            logger.info("%d / %d" % (count, len(catItems)))
+
+          if catItem.reference_name != lastChrom:
+            if lastChrom in chromLengthMap:
+              fdinu.write("%s\t%d\t%d\t%d\n" % (lastChrom, chromLengthMap[lastChrom] - 1, chromLengthMap[lastChrom], 0))
+            lastChrom = catItem.reference_name
+          
+          #logger.info("Processing %s:%d-%d ..." %(catItem.reference_name, catItem.reference_start, catItem.reference_end))
+          tbiter = tb.query(catItem.reference_name, catItem.reference_start, catItem.reference_end)
+          records = [record for record in tbiter]
+          totalCount = 0
+          for record in records:
+            dinucleotide = record[3]
+            diCount = int(record[4])
+            if diCount == 0:
+              diCount = 1
+            totalCount += diCount
+
+          if totalCount > 0:
+            fdinu.write("%s\t%d\t%d\t%d\n" % (catItem.reference_name, catItem.reference_start, catItem.reference_end, totalCount))
 
   targetRmd = outputFilePrefix + ".rmd"
   shutil.copyfile(rmdScript, targetRmd)
 
-  cmd = "R -e \"setwd('%s');library(knitr);rmarkdown::render('%s',params=list(db='%s'));\"" % (os.path.dirname(targetRmd), os.path.basename(targetRmd), dbVersion)
+  targetChromInfoFile =  os.path.join(targetFolder, "chromInfo.txt")
+  shutil.copyfile(chromInfo_file, targetChromInfoFile)
+
+  with open(os.path.join(targetFolder, "options.txt"), "wt") as fout:
+    fout.write("Value\tName\n")
+    fout.write("%s\tsoftVersion\n" % __version__)
+    fout.write("%s\tdbVersion\n" % dbVersion)
+    fout.write("%d\tblock\n" % block)
+
+  cmd = "R -e \"setwd('%s');library(knitr);rmarkdown::render('%s');\"" % (os.path.dirname(targetRmd), os.path.basename(targetRmd))
   runCmd(cmd, logger)
