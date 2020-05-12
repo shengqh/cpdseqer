8bc8e792e08050aa5c0c4c76687bc9798900ac9d
Quanhu Sheng <shengqh@gmail.com>
2020-05-12 15:08:48 -0500

    bugfix

--- cpdseqer/__init__.py
+++ cpdseqer/__init__.py
@@ -1,8 +1,11 @@
+from .background_utils import background
 from .demultiplex_utils import demultiplex
+# from .bowtie2_utils import bowtie2
 from .bam2dinucleotide_utils import bam2dinucleotide
 from .statistic_utils import statistic
-from .position_utils import position
 from .report_utils import report
-from .common_utils import check_file_exists, get_reference_start, runCmd, readFileMap, checkFileMap
+from .fig_genome_utils import fig_genome
+from .fig_position_utils import fig_position
+from .common_utils import check_file_exists, get_reference_start, runCmd, readFileMap, checkFileMap, remove_chr, write_r_script
 
 from .__version__ import __version__
