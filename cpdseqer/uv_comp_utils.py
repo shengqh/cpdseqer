a8d86b9c448e5464c20dfc10917ade3f9e350b4a
Quanhu Sheng <shengqh@gmail.com>
2020-05-15 14:28:47 -0500

    add uv comp sce1

--- cpdseqer/uv_comp_utils.py
+++ cpdseqer/uv_comp_utils.py
@@ -3,7 +3,6 @@ import os.path
 import errno
 import tabix
 import sys
-import shutil
 
 from .common_utils import check_file_exists, get_reference_start, runCmd, readFileMap, checkFileMap, remove_chr, write_rmd_script
 from .__version__ import __version__
@@ -23,23 +22,37 @@ def read_config_file(config_file):
   return(result)
 
 def uv_comp_genome(logger, count_list_file, output_file_prefix, db, count_type):
-  targetFolder = os.path.dirname(output_file_prefix)
-
-  rFunScript = os.path.join( os.path.dirname(__file__), "Rfunctions.R")
-  if not os.path.exists(rFunScript):
-    raise Exception("Cannot find rScript %s" % rFunScript)
-
-  targetRFunScript =  os.path.join(targetFolder, "Rfunctions.R")
-  shutil.copyfile(rFunScript, targetRFunScript)
-
   rScript = os.path.join( os.path.dirname(__file__), "stat_scenarios.Rmd")
-  if not os.path.exists(rScript):
-    raise Exception("Cannot find rScript %s" % rScript)
+
+  countFileMap = readFileMap(count_list_file)
+  countFiles = ",".join([countFileMap[name] for name in sorted(countFileMap.keys())])
 
   options = {
+    "scenario": "sce1",
+    "smp": countFiles,
     "count_list_file": count_list_file,
-    "db": db,
-    "count_type": count_type 
+    "gn": db,
+    "cntType": count_type 
+  }
+
+  targetScript = write_rmd_script(output_file_prefix, rScript, options)
+
+  cmd = "R -e \"setwd('%s');library(knitr);rmarkdown::render('%s');\"" % (os.path.dirname(os.path.abspath(targetScript)), os.path.basename(targetScript))
+  runCmd(cmd, logger)
+
+
+def uv_comp_genome_region(logger, count_list_file, output_file_prefix, db, count_type, coordinate_file):
+  rScript = os.path.join( os.path.dirname(__file__), "stat_scenarios.Rmd")
+
+  countFileMap = readFileMap(count_list_file)
+  countFiles = ",".join([countFileMap[name] for name in sorted(countFileMap.keys())])
+
+  options = {
+    "scenario": "sce1",
+    "smp": countFiles,
+    "count_list_file": count_list_file,
+    "gn": db,
+    "cntType": count_type 
   }
 
   targetScript = write_rmd_script(output_file_prefix, rScript, options)
