41844bdd3d839d2f1ec43d05862ce53899a722d3
Quanhu Sheng <shengqh@gmail.com>
2020-05-28 00:46:30 -0500

    read config file

--- cpdseqer/qc_utils.py
+++ cpdseqer/qc_utils.py
@@ -6,53 +6,36 @@ import sys
 import shutil
 import gzip
 
-from .common_utils import check_file_exists, get_reference_start, runCmd, readFileMap, checkFileMap, remove_chr, write_rmd_script
+from .common_utils import check_file_exists, get_reference_start, runCmd, readFileMap, checkFileMap, remove_chr, write_rmd_script, ConfigItem, read_config_file
 from .__version__ import __version__
 
-def read_config_file(config_file):
-  result = []
-  with open(config_file, "rt") as fin:
-    headers = fin.readline().rstrip().split('\t')
-    name_index = headers.index("Name")
-    file_index = headers.index("File")
-    case_index = headers.index("Case")
-    for line in fin:
-      parts = line.rstrip().split('\t')
-      if len(parts) < len(headers):
-        continue
-      result.append(ConfigItem(parts[name_index], parts[file_index], parts[case_index]))
-  return(result)
+def qc(logger, configFile, project_name, output_prefix, count_type):
+  check_file_exists(configFile)
 
-def qc(logger, list_file, project_name, output_prefix, count_type):
-  check_file_exists(list_file)
+  items = read_config_file(configFile)
+  logger.info("Config:" + str(items))
 
-  files = []
-  with open(list_file) as fh:
-    for line in fh:
-      parts = line.rstrip().split('\t')
-      if len(parts) < 3:
-        raise ArgumentError("list_file %s should contain at least three columns which indcates file name, count file and dinucleotide file without header")
-      check_file_exists(parts[1])
-      check_file_exists(parts[2])
-      check_file_exists(parts[2] + ".tbi")
-      files.append(parts)
+  if len(items) == 0:
+    raise ArgumentError("configFile %s is empty" % configFile)
 
-  if len(files) == 0:
-    raise ArgumentError("list_file doesn't is empty")
-
-  if len(files) == 1:
-    single_qc(logger, files[0][0], files[0][1], files[0][2], output_prefix, count_type)
+  for item in items:
+    check_file_exists(item.dinucleotide_file)
+    check_file_exists(item.index_file)
+    check_file_exists(item.count_file)
+  
+  if len(items) == 1:
+    single_qc(logger, items[0], output_prefix, count_type)
   else:
-    multi_qc(logger, project_name, list_file, output_prefix, count_type)
+    multi_qc(logger, project_name, items, output_prefix, count_type)
 
 
-def single_qc(logger, sample_name, count_file, dinucleotide_file, output_prefix, count_type):
+def single_qc(logger, config_item, output_prefix, count_type):
   targetFolder = os.path.dirname(output_prefix)
 
   options = {
-    "sample": sample_name,
-    "bgz": dinucleotide_file,
-    "cnt": count_file,
+    "sample": config_item.name,
+    "bgz": config_item.dinucleotide_file,
+    "cnt": config_item.count_file,
     "count_type": count_type 
   }
 
@@ -63,13 +46,18 @@ def single_qc(logger, sample_name, count_file, dinucleotide_file, output_prefix,
   runCmd(cmd, logger)
 
 
-def multi_qc(logger, project_name, list_file, output_prefix, count_type):
+def multi_qc(logger, project_name, config_items, output_prefix, count_type):
   targetFolder = os.path.dirname(output_prefix)
+
+  targetConfigFile = output_prefix + ".config"
+  with open(targetConfigFile, "wt") as fout:
+    for item in config_items:
+      fout.write("%s\t%s\t%s\n" % (item.name, item.count_file, item.dinucleotide_file))
 
   rScript = os.path.join( os.path.dirname(__file__), "multiQC.Rmd")
   
   options = {
-    "smps":	list_file,
+    "smps":	os.path.basename(targetConfigFile),
     "dinuc": "DINUC4",
     "cntType": count_type,
     "exp": project_name
