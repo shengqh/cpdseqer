d2a5d4b02663304c69fda2c41f9e6cb6f2f0ce02
Quanhu Sheng <shengqh@gmail.com>
2020-08-26 11:15:55 -0500

    remove refcolumn

--- cpdseqer/size_factor.r
+++ cpdseqer/size_factor.r
@@ -8,7 +8,7 @@ library(edgeR)
 
 bedCM <- fread(count_file,header=T,data.table=F) # bed Count Matrix
 bedCM<-bedCM[,-1]
-normFac <- calcNormFactors(bedCM,method='TMM',refColumn=1)
+normFac <- calcNormFactors(bedCM,method='TMM')
 librarysize = colSums(bedCM)
 sf<-data.frame("Sample"=names(normFac), "SizeFactor"=normFac, "LibrarySize"=librarysize)
 write.table(sf, file=output_file, sep="\t", row.names=F, col.names=T, quote=F)
